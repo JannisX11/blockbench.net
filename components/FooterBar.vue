ffbba787650baf19e0c87250f7c11a5ef8c65ba6
JannisX11 <jannis4236@gmail.com>
2021-06-14 15:15:58 +0200

    Include netlify badge

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,6 +1,9 @@
 <template>
 	<footer id="footer">
 		<div id="footer_content">
+			<a href="https://www.netlify.com" target="_blank" rel="noopener">
+				This site is powered by <img id="netlify_logo" src="~assets/netlify-full-logo-dark.svg" alt="Deploys by Netlify" />
+			</a>
 			<nav>
 				<nuxt-link to='/about'>About</nuxt-link>
 				<nuxt-link to='/imprint'>Imprint & Privacy Policy</nuxt-link>
@@ -22,14 +25,13 @@ export default {
 <style scoped>
 
 	#footer {
-		min-height: 30px;
+		min-height: 42px;
+		padding: 10px;
 		background-color: var(--dark-ui);
 		color: var(--dark-subtle);
 	}
 	#footer_content {
 		max-width: var(--max-page-width);
-		padding-top: 5px;
-		padding-bottom: 0;
 		margin: auto;
 	}
 	nav {
@@ -41,7 +43,21 @@ export default {
 	a:hover {
 		color: var(--dark-hover);
 	}
+	#netlify_logo {
+		vertical-align: text-top;
+		height: 20px;
+	}
 
 	@media only screen and (max-width: 600px) {
+		
+		#footer {
+			text-align: center;
+		}
+		nav {
+			float: none;
+			display: flex;
+			justify-content: space-around;
+			margin-top: 2px;
+		}
 	}
 </style>>
