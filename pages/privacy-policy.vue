10ed4012496195ca06e009f0a9d924a752ef8282
JannisX11 <jannis4236@gmail.com>
2023-04-27 17:32:29 +0200

    Update privacy policy

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -4,7 +4,7 @@
 			
 			<h1>Privacy Policy</h1>
 			
-			<p>Last updated: April 21, 2023</p>
+			<p>Last updated: April 27, 2023</p>
 			<p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
 			<p>We use certain data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
 			
