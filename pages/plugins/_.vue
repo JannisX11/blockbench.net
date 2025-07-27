69332f1751e8981ae18b6345d7c734dec1b9579b
JannisX11 <jannis4236@gmail.com>
2025-07-27 23:00:58 +0200

    Display plugin about files and deprecation notes

--- pages/plugins/_.vue
+++ pages/plugins/_.vue
@@ -12,12 +12,14 @@
 				<p class="author">by {{ plugin.author }}</p>
 				<p class="description">{{ plugin.description }}</p>
 
+				<p class="deprecation_note" v-if="plugin.deprecation_note">{{ plugin.deprecation_note }}</p>
+
 				<PluginTags :tags="plugin.tags" />
 
-				<template v-if="about">
+				<div v-if="about" class="about">
 					<h4>About</h4>
 					<p class="about" v-html="about.html"></p>
-				</template>
+				</div>
 
 				<p class="variant" v-if="plugin.variant == 'desktop'">Only for the desktop app</p>
 				<p class="variant" v-if="plugin.variant == 'web'">Only for the web app</p>
@@ -44,6 +46,35 @@
 <script>
 
 import Markdown from '@nuxt/markdown'
+
+/**
+ * Compare two versions
+ * @param {string} version1 
+ * @param {string} version2 
+ * @returns Whether version1 is higher/newer than version2
+ */
+ function compareVersions(version1, version2) {
+	var arr1 = version1.split(/[.-]/);
+	var arr2 = version2.split(/[.-]/);
+	var i = 0;
+	var num1 = 0;
+	var num2 = 0;
+	while (i < Math.max(arr1.length, arr2.length)) {
+		num1 = arr1[i];
+		num2 = arr2[i];
+		if (num1 == 'beta') num1 = -1;
+		if (num2 == 'beta') num2 = -1;
+		num1 = parseInt(num1) || 0;
+		num2 = parseInt(num2) || 0;
+		if (num1 > num2) {
+			return true;
+		} else if (num1 < num2) {
+			return false
+		}
+		i++;
+	}
+	return false;
+}
 
 export default {
 	async asyncData({params}) {
@@ -80,10 +111,26 @@ export default {
 			}
 		}
 
-		let about;
-		if (plugins[plugin_id].about) {
+		const plugin = plugins[plugin_id];
+		if (plugin.min_version && !compareVersions('4.8.0', plugin.min_version)) {
+			plugin.new_repository_format = true;
+		}
+
+		let about = plugin.about;
+		if (!about && plugin.new_repository_format) {
+			const api_path = 'https://cdn.jsdelivr.net/gh/JannisX11/blockbench-plugins/plugins';
+			let url = `${api_path}/${plugin_id}/about.md`;
+			let result = await fetch(url).catch(() => {
+				console.error('about.md missing for plugin ' + plugin_id);
+			});
+			if (result && result.ok) {
+				about = await result.text();
+			}
+		}
+
+		if (about) {
 			let md = new Markdown({ toc: false, sanitize: false });
-			about = await md.toMarkup(plugins[plugin_id].about);
+			about = await md.toMarkup(about);
 		}
 
 		return {
@@ -91,7 +138,7 @@ export default {
 			plugin_id,
 			last_modified,
 			about,
-			plugin: plugins[plugin_id]
+			plugin
 		};
 	},
 	head() {
@@ -134,6 +181,12 @@ export default {
 	.plugin .author {
 		color: var(--light-subtle);
 	}
+	.plugin .deprecation_note {
+		margin-bottom: 8px;
+		padding: 10px 12px;
+		background-color: var(--light-background);
+		border-left: 10px solid #f5a130e0;
+	}
 	.plugin .variant {
 		color: var(--light-subtle);
 	}
@@ -169,5 +222,21 @@ export default {
 	.plugin_tag_list li.dark_text {
 		color: black;
 	}
+	div.about {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
 
+</style>
+<style>
+
+	p.about i.icon {
+		display: none;
+	}
+	p.about img {
+		max-width: 100%;
+	}
+	p.about h1, p.about h2 {
+		padding-bottom: 12px;
+	}
 </style>
