880d8006a3d765799734858a05469ceb28bb8f8d
JannisX11 <jannis4236@gmail.com>
2025-10-13 20:33:27 +0200

    Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -17,10 +17,22 @@
 			</p>
 
 			<h2>Contact</h2>
-			<p>For help with Blockbench, please use the help channels on the Blockbench Discord server.</p>
-			<p><a target="_blank" rel="noopener" href="https://discord.gg/blockbench">Discord Server</a></p>
+			<p>
+				For help with Blockbench, please use the help channels on the Blockbench Discord server:
+				<a target="_blank" rel="noopener" href="https://discord.gg/blockbench">Discord Server</a>
+			</p>
 			
-			<p>Or visit the subreddit: <a target="_blank" rel="noopener" href="https://www.reddit.com/r/Blockbench/">r/Blockbench</a></p>
+			<p>
+				Or visit the subreddit:
+				<a target="_blank" rel="noopener" href="https://www.reddit.com/r/Blockbench/">r/Blockbench</a>
+			</p>
+
+			<p>
+				You found a bug or have an idea for a Blockbench feature? Visit the GitHub issue tracker:
+				<a target="_blank" rel="noopener" href="https://github.com/JannisX11/blockbench/issues">JannisX11/Blockbench</a>
+			</p>
+
+			<p>Email: info (at) blockbench dot net<br />Please note that we do not provide support via email. If you have a question or need help, please use the links listed above.</p>
 
 		</content>
 	</div>
